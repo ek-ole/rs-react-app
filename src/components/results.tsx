3892b81ef164d5e562de7b81663a178b33f4e8b6
ek-ole <ek_34@mail.ru>
2025-07-12 15:22:28 +0300

    feat: add some styles

--- src/components/results.tsx
+++ src/components/results.tsx
@@ -10,17 +10,19 @@ class Results extends React.Component<ResultsProps> {
   render() {
     const { characters } = this.props;
     return (
-      <div className="mx-auto mt-6 flex w-full max-w-2xl flex-col items-center">
+      <div className="mx-auto mt-6 flex w-full max-w-2xl flex-col items-center rounded-xl border-4 p-4">
         <h2>Characters</h2>
-        {characters.map((character) => (
-          <div
-            key={character.id}
-            className="my-2 flex w-full items-center gap-2 rounded-xl border-4 p-4"
-          >
-            <h3>{character.name}</h3>
-            <p>{character.description}</p>
-          </div>
-        ))}
+        <div className="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3">
+          {characters.map((character) => (
+            <div
+              key={character.id}
+              className="shadow-foreground/50 my-2 items-center gap-2 rounded-xl border-4 p-4 shadow-lg transition-shadow hover:shadow-md"
+            >
+              <h3 className="mb-2 text-lg font-semibold">{character.name}</h3>
+              <p className="text-sm">{character.description}</p>
+            </div>
+          ))}
+        </div>
       </div>
     );
   }
