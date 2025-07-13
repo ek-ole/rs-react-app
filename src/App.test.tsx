917aee62f57b579a1aa874b7be5e10da04157c31
ek-ole <ek_34@mail.ru>
2025-07-13 20:49:32 +0300

    fix: fix test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,14 @@
 import { render, screen } from '@testing-library/react';
-import { test, expect } from 'vitest';
+import { test, expect, vi } from 'vitest';
 
 import App from './App';
 
+vi.mock('lottie-react', () => ({
+  default: () => <div>Mock Lottie</div>,
+  __esModule: true,
+}));
+
 test('renders hello text', () => {
   render(<App />);
-  expect(screen.getByText(/hello rick & morty/i)).toBeInTheDocument();
+  expect(screen.getByText(/rick & morty/i)).toBeInTheDocument();
 });
