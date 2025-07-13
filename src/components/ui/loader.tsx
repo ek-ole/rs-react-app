c57f23ce37cf60b086ba7124569b05bd5bdf6c22
ek-ole <ek_34@mail.ru>
2025-07-13 13:56:51 +0300

    feat: add lottie animation for loading

--- src/components/ui/loader.tsx
+++ src/components/ui/loader.tsx
@@ -1,11 +1,18 @@
+import Lottie from 'lottie-react';
 import React from 'react';
+
+import animationData from 'public/animations/morty-dance-loader.json';
 
 export class Loader extends React.Component {
   render() {
     return (
-      <div className="flex flex-col items-center justify-center py-8">
-        <div className="h-12 w-12 animate-spin rounded-full border-4 border-t-transparent" />
-        <p className="font-medium">Loading...</p>
+      <div className="fixed inset-0 z-50 flex items-center justify-center">
+        <div className="flex flex-col items-center">
+          <div className="h-40 w-40">
+            <Lottie animationData={animationData} loop={true} autoplay={true} />
+          </div>
+          <p className="p-2 font-medium">Loading...</p>
+        </div>
       </div>
     );
   }
