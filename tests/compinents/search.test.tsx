bb64de38dc120736bc11258c2856d92a54bf38ad
ek-ole <ek_34@mail.ru>
2025-07-19 14:54:09 +0300

    test: fix test for search

--- tests/compinents/search.test.tsx
+++ tests/compinents/search.test.tsx
@@ -1,35 +1,38 @@
-vi.mock('@/services/storage', () => ({
-  LocalStorageService: {
-    getSearchTerm: vi.fn(() => 'initial value'),
-    setSearchTerm: vi.fn(),
-    clearSearchTerm: vi.fn(),
-  },
-}));
-
 import { render, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 
 import Search from '@/components/search';
 
+vi.mock('@/services/storage', () => ({
+  LocalStorageService: {
+    getSearchTerm: () => 'initial value',
+    setSearchTerm: vi.fn(),
+    clearSearchTerm: vi.fn(),
+  },
+}));
+
 describe('Search Component', () => {
+  const user = userEvent.setup();
+  const mockOnSearch = vi.fn();
+
   beforeEach(() => {
-    localStorage.clear();
     vi.clearAllMocks();
   });
-  it('should render input, button', () => {
-    render(<Search onSearch={vi.fn()} />);
+
+  it('should render input', () => {
+    render(<Search onSearch={mockOnSearch} />);
     const input = screen.getByRole('searchbox');
     expect(input).toBeInTheDocument();
     expect(input).toHaveValue('initial value');
+  });
 
+  it('should render button', () => {
+    render(<Search onSearch={mockOnSearch} />);
     const button = screen.getByRole('button', { name: /search/i });
     expect(button).toBeInTheDocument();
   });
 
   it('should trim input and call onSearch', async () => {
-    const mockOnSearch = vi.fn();
-    const user = userEvent.setup();
-
     render(<Search onSearch={mockOnSearch} />);
 
     const input = screen.getByRole('searchbox');
