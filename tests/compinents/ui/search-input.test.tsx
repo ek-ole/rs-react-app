7deadb8e54fb154cc4117908a8eba9b189764db6
ek-ole <ek_34@mail.ru>
2025-07-19 14:05:09 +0300

    test: add test for search-input with default placeholder

--- tests/compinents/ui/search-input.test.tsx
+++ tests/compinents/ui/search-input.test.tsx
@@ -18,6 +18,12 @@ describe('SearchInput', () => {
     expect(input).toHaveAttribute('placeholder', 'Test placeholder');
   });
 
+  it('should use default placeholder if none provided', () => {
+    render(<SearchInput {...defaultProps} placeholder={undefined} />);
+    const input = screen.getByRole('searchbox');
+    expect(input).toHaveAttribute('placeholder', 'Search character...');
+  });
+
   it('should display the passed value', () => {
     render(<SearchInput {...defaultProps} value="Rick" />);
     expect(screen.getByRole('searchbox')).toHaveValue('Rick');
