498b2495b29b8b70c5fd347a7c6c9c1426ffd42e
ek-ole <ek_34@mail.ru>
2025-07-21 02:28:42 +0300

    test: add test for app

--- tests/App.test.tsx
+++ tests/App.test.tsx
@@ -1,12 +1,47 @@
 import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 
 import App from '@/App';
+import { AppService } from '@/services/app-service';
 
-describe('header', () => {
+vi.mock('@/services/app-service');
+vi.mock('@/components/results', () => ({
+  default: () => <div data-testid="mock-results" />,
+}));
+
+describe('App', () => {
+  beforeEach(() => {
+    vi.spyOn(AppService, 'loadCharacters').mockResolvedValue([]);
+    vi.spyOn(AppService, 'saveSearchTerm').mockImplementation(() => {});
+  });
+
+  const mockCharacters = [
+    { id: 1, name: 'Rick', description: 'Human', image: 'rick.png' },
+    { id: 2, name: 'Morty', description: 'Human', image: 'morty.png' },
+  ];
+
   it('should render header text', () => {
     render(<App />);
     const heading = screen.getByRole('heading');
     expect(heading).toBeInTheDocument();
     expect(heading).toHaveTextContent(/rick & morty/i);
+  });
+
+  it('should update character list after search', async () => {
+    const loadCharactersSpy = vi
+      .spyOn(AppService, 'loadCharacters')
+      .mockResolvedValue(mockCharacters);
+
+    const user = userEvent.setup();
+
+    render(<App />);
+
+    const input = screen.getByRole('searchbox');
+    const button = screen.getByRole('button');
+
+    await user.type(input, 'Rick');
+    await user.click(button);
+
+    expect(loadCharactersSpy).toHaveBeenCalledWith('Rick');
   });
 });
