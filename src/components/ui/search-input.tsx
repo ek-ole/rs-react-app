59ad83b952beaf96a0c4360e94642c13e370ca05
ek-ole <ek_34@mail.ru>
2025-07-24 11:28:35 +0300

    refactor: change classes to functions in search-input.tsx

--- src/components/ui/search-input.tsx
+++ src/components/ui/search-input.tsx
@@ -1,5 +1,3 @@
-import { Component } from 'react';
-
 import { cn } from '@/utils/cn';
 
 type SearchInputProps = {
@@ -8,20 +6,18 @@ type SearchInputProps = {
   placeholder?: string;
 };
 
-export class SearchInput extends Component<SearchInputProps> {
-  render() {
-    return (
-      <input
-        type="search"
-        value={this.props.value}
-        onChange={(e) => this.props.onChange(e.target.value)}
-        placeholder={this.props.placeholder || 'Search character...'}
-        className={cn(
-          'bg-primary-light w-full flex-1',
-          'rounded-xl px-2 py-1',
-          'focus:outline-none sm:px-4',
-        )}
-      />
-    );
-  }
+export function SearchInput({ value, onChange, placeholder }: SearchInputProps) {
+  return (
+    <input
+      type="search"
+      value={value}
+      onChange={(e) => onChange(e.target.value)}
+      placeholder={placeholder || 'Search character...'}
+      className={cn(
+        'bg-primary-light w-full flex-1',
+        'rounded-xl px-2 py-1',
+        'focus:outline-none sm:px-4',
+      )}
+    />
+  );
 }
