0ed41cce7ae8c8c9a45ae1ed411f6a3dcc75e3c1
ek-ole <ek_34@mail.ru>
2025-07-24 13:56:58 +0300

    refactor: change classes to functions in loader.tsx

--- src/components/ui/loader.tsx
+++ src/components/ui/loader.tsx
@@ -1,23 +1,19 @@
-import { Component } from 'react';
-
-export class Loader extends Component {
-  render() {
-    return (
-      <div className="fixed inset-0 z-50 flex items-center justify-center">
-        <div className="flex h-[25vh] animate-[fadeIn_0.3s_ease-in-out_forwards] flex-col items-center">
-          <video
-            autoPlay
-            loop
-            muted
-            playsInline
-            className="h-full w-full object-contain"
-            data-testid="loader-video"
-          >
-            <source src="/morty-dance-loader.webm" type="video/webm" data-testid="source-video" />
-          </video>
-          <p className="p-2 font-medium">Loading...</p>
-        </div>
+export function Loader() {
+  return (
+    <div className="fixed inset-0 z-50 flex items-center justify-center">
+      <div className="flex h-[25vh] animate-[fadeIn_0.3s_ease-in-out_forwards] flex-col items-center">
+        <video
+          autoPlay
+          loop
+          muted
+          playsInline
+          className="h-full w-full object-contain"
+          data-testid="loader-video"
+        >
+          <source src="/morty-dance-loader.webm" type="video/webm" data-testid="source-video" />
+        </video>
+        <p className="p-2 font-medium">Loading...</p>
       </div>
-    );
-  }
+    </div>
+  );
 }
