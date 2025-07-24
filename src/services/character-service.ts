df553a5f3870f23a80d779bfeb8e5eca6bc38860
ek-ole <ek_34@mail.ru>
2025-07-24 14:57:07 +0300

    refactor: change classes to functions in character-service.ts

--- src/services/character-service.ts
+++ src/services/character-service.ts
@@ -1,10 +1,12 @@
 import { mapApiToCharacter } from '@/api/map-characters';
 import { fetchCharacters } from '@/api/rick-and-morty-api';
 
-export class CharacterService {
-  static async loadCharacters(searchTerm = '') {
-    const data = await fetchCharacters(searchTerm);
-    const characters = data.results.map(mapApiToCharacter);
-    return characters;
-  }
+export async function loadCharacters(searchTerm = '') {
+  const data = await fetchCharacters(searchTerm);
+  const characters = data.results.map(mapApiToCharacter);
+  return characters;
 }
+
+export const CharacterService = {
+  loadCharacters,
+};
