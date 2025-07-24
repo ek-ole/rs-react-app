5cf12b3225f7da16248060bd06a4f661f5b0120a
ek-ole <ek_34@mail.ru>
2025-07-24 11:33:06 +0300

    refactor: change classes to functions in results.tsx

--- src/components/results.tsx
+++ src/components/results.tsx
@@ -1,5 +1,3 @@
-import { Component } from 'react';
-
 import type { Character } from '@/types/character';
 import { cn } from '@/utils/cn';
 
@@ -9,24 +7,22 @@ type ResultsProps = {
   characters: Character[];
 };
 
-class Results extends Component<ResultsProps> {
-  render() {
-    return (
-      <div
-        className={cn(
-          'mx-auto mt-6 flex w-full max-w-6xl',
-          'flex-col items-center rounded-xl border-4 p-4',
-        )}
-      >
-        <h2>Characters</h2>
-        <div className="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-4">
-          {this.props.characters.map((character) => (
-            <CharacterCard key={character.id} character={character} />
-          ))}
-        </div>
+function Results({ characters }: ResultsProps) {
+  return (
+    <div
+      className={cn(
+        'mx-auto mt-6 flex w-full max-w-6xl',
+        'flex-col items-center rounded-xl border-4 p-4',
+      )}
+    >
+      <h2>Characters</h2>
+      <div className="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-4">
+        {characters.map((character) => (
+          <CharacterCard key={character.id} character={character} />
+        ))}
       </div>
-    );
-  }
+    </div>
+  );
 }
 
 export default Results;
