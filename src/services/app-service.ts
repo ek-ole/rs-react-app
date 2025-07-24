bae0ee9711385b0e55ab6cae7b692d144227a123
ek-ole <ek_34@mail.ru>
2025-07-24 14:40:12 +0300

    refactor: change classes to functions in app-service.ts

--- src/services/app-service.ts
+++ src/services/app-service.ts
@@ -3,20 +3,24 @@ import { ApiErrorHandler } from '@/api/api-eror-handler';
 import { CharacterService } from './character-service';
 import { LocalStorageService } from './storage';
 
-export class AppService {
-  static async loadCharacters(searchTerm: string) {
-    try {
-      return await CharacterService.loadCharacters(searchTerm);
-    } catch (error) {
-      throw new Error(ApiErrorHandler.getErrorMessage(error));
-    }
-  }
-
-  static getInitialSearchTerm() {
-    return LocalStorageService.getSearchTerm();
-  }
-
-  static saveSearchTerm(term: string) {
-    LocalStorageService.setSearchTerm(term);
+export async function loadCharacters(searchTerm: string) {
+  try {
+    return await CharacterService.loadCharacters(searchTerm);
+  } catch (error) {
+    throw new Error(ApiErrorHandler.getErrorMessage(error));
   }
 }
+
+export function getInitialSearchTerm() {
+  return LocalStorageService.getSearchTerm();
+}
+
+export function saveSearchTerm(term: string) {
+  LocalStorageService.setSearchTerm(term);
+}
+
+export const AppService = {
+  loadCharacters,
+  getInitialSearchTerm,
+  saveSearchTerm,
+};
