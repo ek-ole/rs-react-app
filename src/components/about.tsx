c36d55c9294b483391437a147fc99a8922482f8f
ek-ole <ek_34@mail.ru>
2025-07-25 17:45:19 +0300

    feat: create about page

--- src/components/about.tsx
+++ src/components/about.tsx
@@ -1,8 +1,46 @@
+import { Link } from 'react-router-dom';
+
+import { cn } from '@/utils/cn';
+
 function About() {
   return (
-    <>
-      <h2 className="">about</h2>
-    </>
+    <div className="flex min-h-[60vh] items-center justify-center p-4 md:p-14">
+      <div
+        className={cn(
+          'mx-auto flex w-full max-w-4xl flex-col',
+          'items-center rounded-xl border-4 p-4',
+        )}
+      >
+        <h2 className="mb-6 text-center text-2xl font-bold">About the Project</h2>
+
+        <div className="mb-8 space-y-4 text-center">
+          <p>This is a React application for exploring Rick and Morty characters.</p>
+          <p className="flex items-center whitespace-nowrap">
+            Created by Ekaterina Dmitrenko as part of&nbsp;
+            <Link
+              to="https://rs.school/courses/reactjs"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="semi-bold flex items-center whitespace-nowrap hover:underline"
+            >
+              RS School React Course
+              <img
+                src="/rss-logo.svg"
+                alt="RS School"
+                className="ml-2 h-5 w-5 transition-transform duration-300 hover:scale-110"
+              />
+            </Link>
+          </p>
+        </div>
+
+        <img
+          src="/about-image.webp"
+          alt="About project"
+          className="mb-6 h-[50vh] rounded-2xl object-contain"
+          loading="lazy"
+        />
+      </div>
+    </div>
   );
 }
 
