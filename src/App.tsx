4805080c500d828ef4221f5cc905904407cabbed
ek-ole <ek_34@mail.ru>
2025-07-27 18:18:36 +0300

    refactor: move logic out of app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,72 +1,12 @@
-import { useEffect, useState } from 'react';
-import { useSearchParams } from 'react-router-dom';
-
-import { getErrorMessage } from './api/api-error-handler';
 import { Pagination } from './components/pagination';
 import Results from './components/results';
 import Search from './components/search';
 import { Loader } from './components/ui/loader';
 import { NotFound } from './components/ui/not-found';
-import useLocalStorage from './hooks/use-local-storage';
-import { loadAndProcessCharacters } from './services/app-service';
-import { SEARCH_TERM_KEY } from './services/constants';
-import type { AppState } from './types/app';
+import { useCharacters } from './hooks/use-characters';
 
 function App() {
-  const [searchTerm, setSearchTerm] = useLocalStorage(SEARCH_TERM_KEY, '');
-  const [state, setState] = useState<Omit<AppState, 'searchValue'>>({
-    characters: [],
-    isLoading: false,
-    error: null,
-    totalPages: 1,
-  });
-  const [currentPage, setCurrentPage] = useState(1);
-
-  const [searchParams, setSearchParams] = useSearchParams();
-
-  const loadData = async (searchTerm = '', page = 1) => {
-    setState((prev) => ({ ...prev, isLoading: true, error: null }));
-    try {
-      const { characters, totalPages } = await loadAndProcessCharacters(searchTerm, page);
-      setState((prev) => ({ ...prev, characters, totalPages }));
-    } catch (error) {
-      setState((prev) => ({
-        ...prev,
-        error: getErrorMessage(error),
-        characters: [],
-        totalPages: 1,
-      }));
-    } finally {
-      setState((prev) => ({ ...prev, isLoading: false }));
-    }
-  };
-
-  const handleSearch = (term: string) => {
-    const trimmedValue = term.trim();
-    setSearchTerm(trimmedValue);
-
-    const params = new URLSearchParams(searchParams);
-    params.set('search', trimmedValue);
-    params.set('page', '1');
-    setSearchParams(params);
-    void loadData(trimmedValue, 1);
-  };
-
-  const handlePageChange = (page: number) => {
-    setCurrentPage(page);
-    searchParams.set('page', String(page));
-    setSearchParams(searchParams);
-    void loadData(searchTerm, page);
-  };
-
-  useEffect(() => {
-    const searchTerm = searchParams.get('search') || '';
-    const page = Number(searchParams.get('page')) || 1;
-
-    setSearchTerm(searchTerm);
-    setCurrentPage(page);
-    void loadData(searchTerm, page);
-  }, [searchParams, searchTerm, setSearchTerm]);
+  const { state, currentPage, handleSearch, handlePageChange } = useCharacters();
 
   return (
     <div className="mx-auto my-6 flex w-full flex-col items-center px-8 py-4">
