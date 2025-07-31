9e4aed6d3851c1058a31bc7245abc4d7b93af6e2
ek-ole <ek_34@mail.ru>
2025-08-01 01:23:49 +0300

    feat: add theme change

--- src/main.tsx
+++ src/main.tsx
@@ -4,12 +4,15 @@ import '@/styles/index.css';
 import { RouterProvider } from 'react-router-dom';
 
 import { ErrorBoundary } from './components/error-boundary.tsx';
+import { ThemeProvider } from './components/ui/theme-context.tsx';
 import { router } from './routes.tsx';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <ErrorBoundary>
-      <RouterProvider router={router} />
-    </ErrorBoundary>
+    <ThemeProvider>
+      <ErrorBoundary>
+        <RouterProvider router={router} />
+      </ErrorBoundary>
+    </ThemeProvider>
   </StrictMode>,
 );
