6255c8da75e5ce305c9447c6ddaee4abdc1803c0
ek-ole <ek_34@mail.ru>
2025-08-07 18:17:16 +0300

    feat: create api slice

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,15 +1,34 @@
 import { configureStore } from '@reduxjs/toolkit';
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 import { useDispatch } from 'react-redux';
 
 import selectedReducer from '@/services/selected-characters';
+import type { ApiResponse } from '@/types/api';
+
+const rickAndMortyApi = createApi({
+  reducerPath: 'rickAndMortyApi',
+  baseQuery: fetchBaseQuery({
+    baseUrl: 'https://rickandmortyapi.com/api/',
+  }),
+  endpoints: (builder) => ({
+    getCharacters: builder.query<ApiResponse, { name?: string; page: number }>({
+      query: ({ name, page }) => ({
+        url: 'character',
+        params: { name, page },
+      }),
+    }),
+  }),
+});
 
 export const store = configureStore({
   reducer: {
     selectedCharacters: selectedReducer,
+    [rickAndMortyApi.reducerPath]: rickAndMortyApi.reducer,
   },
-  devTools: true,
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(rickAndMortyApi.middleware),
 });
 
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
 export const useAppDispatch = () => useDispatch<AppDispatch>();
+export const { useGetCharactersQuery } = rickAndMortyApi;
