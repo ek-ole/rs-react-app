544332c7ef84d141f3238aee69aa39faf16a3240
ek-ole <ek_34@mail.ru>
2025-08-26 19:48:52 +0300

    refactor: fix styles

--- performance/src/components/home-page.tsx
+++ performance/src/components/home-page.tsx
@@ -31,19 +31,49 @@ function HomePage() {
           </div>
           <div className="mb-8 space-y-4 text-center">
             <p>
-              This is a React application for exploring techniques and best practices to optimize
-              the performance of React applications.
+              This CO2 emissions data analysis app was created while learning about methods and best
+              practices for optimizing the performance of React apps.
             </p>
           </div>
+          <button           
+            className={cn(
+              'hover:bg-shadow hover:text-primary-light',
+              'hover:border-shadow m-8 cursor-pointer',
+              'rounded-xl border-3 px-2 font-medium',
+              'transition-colors duration-400 sm:border-4 sm:px-4 sm:py-2',
+            )}
+          >
+            Button
+          </button>
         </div>
       </div>
       <p className="absolute bottom-2 flex w-full flex-wrap items-center justify-center">
-        Created by Ekaterina Dmitrenko as part RS School React Course
-        <img
-          src="/rss-logo.svg"
-          alt="RS School"
-          className="ml-2 h-5 w-5 transition-transform duration-300 hover:scale-110"
-        />
+        Created by{' '}
+        <a
+          href="https://github.com/ek-ole"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="mx-1 font-semibold transition-transform duration-300 hover:scale-103"
+        >
+          Ekaterina Dmitrenko
+        </a>{' '}
+        as part of{' '}
+        <a
+          href="https://rs.school/courses/reactjs"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="mx-1 font-semibold transition-transform duration-300 hover:scale-103"
+        >
+          RS School React Course
+        </a>
+        <a
+          href="https://rs.school/"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="ml-2 transition-transform duration-300 hover:scale-110"
+        >
+          <img src="/rss-logo.svg" alt="RS School" className="h-5 w-5" />
+        </a>
       </p>
     </>
   );
